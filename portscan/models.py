ad6f8636d6241ad14bb06616b51f0aeb051b4ea2
LittleLion2022 <2715158962@qq.com>
2023-03-30 10:43:56 +0800

    20230330 端口扫描 将结果保存在数据库中

--- portscan/models.py
+++ portscan/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Ports(models.Model):
+    username = models.CharField(max_length=20)
+    ip = models.GenericIPAddressField()
+    portid = models.IntegerField()
+    service = models.CharField(max_length=30,default='unknown')
+    datetime = models.DateTimeField()
--- portscan/views.py
+++ portscan/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-import re,json,nmap3
+from portscan import models
+import re,json,nmap3,datetime
 
 # Create your views here.
 def scanner(request):
@@ -7,7 +8,16 @@ def scanner(request):
         return redirect('/')
     if request.method == 'POST':
         if cheak_ip(request.POST.get('ip').strip()):
-            pass
+            host = request.POST.get('ip').strip()
+            results,time =  host_scan(host)
+            for result in results:
+                new_result = models.Ports()
+                new_result.username = request.session['username']
+                new_result.ip = host
+                new_result.portid = result['portid']
+                new_result.service = result['service']['name']
+                new_result.datetime = time
+                new_result.save()
         else:
             message = 'IP地址格式非法'
             return render(request,'port-scan.html',locals())
@@ -24,5 +34,8 @@ def cheak_ip(string):
 def host_scan(host):
     nmap = nmap3.NmapHostDiscovery()
     result = nmap.nmap_portscan_only(host)
-
-    return 
+    ports = result[host]['ports']
+    time = datetime.datetime.now()
+    for i in ports:
+        print(f'{i["protocol"]}\t{i["portid"]}\t{i["state"]}\t{i["service"]["name"]}')
+    return ports,time
