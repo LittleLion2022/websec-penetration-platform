931106de053249b9b0bb8818bfa6ca4e02fcdcc9
LittleLion2022 <2715158962@qq.com>
2023-04-19 19:19:02 +0800

    20230419 完成子域名爆破模块

--- dnssearch/models.py
+++ dnssearch/models.py
@@ -5,4 +5,5 @@ class Domains(models.Model):
     username = models.CharField(max_length=20)
     domain = models.CharField(max_length=100)
     subdomain = models.CharField(max_length=100,default='')
+    count = models.IntegerField(default=0)
     datetime = models.DateTimeField()
--- dnssearch/views.py
+++ dnssearch/views.py
@@ -3,11 +3,15 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from dnssearch import models
 from api import models as api_models
-import re,requests
+import re,json,requests
 # Create your views here.
 def scanner(request):
     if not request.session.get('is_login', None):
         return redirect('/')
+    domain_list = models.Domains.objects.filter(username=request.session['username']).order_by("-datetime")
+    domain_list = Paginator(domain_list, 7)
+    page = request.GET.get('page')
+    domain_list = domain_list.get_page(page)
     if request.method == 'POST':
         domain = request.POST.get('domain').strip()
         if check_domain(domain):
@@ -23,11 +27,26 @@ def scanner(request):
             }
             response = requests.get(url,headers=headers)
             print(response.text)
-            print(headers['x-apikey'])
+            result = json.loads(response.text)
+            if  response.status_code == 200:
+                time = time = timezone.now()
+                count = int(result['meta']['count'])
+                for result in result['data']:
+                    print(result['id'])
+                    new_result = models.Domains()
+                    new_result.username = request.session['username']
+                    new_result.domain = domain
+                    new_result.subdomain = result['id']
+                    new_result.count = count
+                    new_result.datetime = time
+                    new_result.save()
+            else:
+                message = result["error"]["message"]
+                return render(request,'dns-search.html',locals())
         else:
             message = '域名格式非法'
             return render(request,'dns-search.html',locals())
-    return render(request,'dns-search.html')
+    return render(request,'dns-search.html',locals())
 
 def check_domain(string):
     domain_pattern = '(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
